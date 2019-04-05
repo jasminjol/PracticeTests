556b387185a12d964ce02758069d825a02917f01
jasminjol <48532317+jasminjol@users.noreply.github.com>
2019-04-05 15:20:35 -0700

    Add files via upload

---
+++ RemoveDupCharsFromString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDupCharsFromString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = String.Join("", input.Distinct());
+            Console.WriteLine("String with duplicate characters removed - " + output);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RotateArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray= new int[6] { 1,2,3,4,5,6 };
+            RotateArray(inputArray, 2);
+        }
+
+        public static void RotateArray(int[] input, int pivot)
+        {
+            int[] tempArray = new int[pivot];
+
+            Array.Copy(input, tempArray, pivot);
+
+            int pos = 0;
+
+            int[] target = new int[input.Length];
+
+            for (int i = pivot; i< input.Length; i++)
+            {
+               target[pos] = input[i];
+                pos++;
+
+            }
+
+            Array.Copy(tempArray, 0,  target, target.Length-pivot, pivot);
+
+            for (int len = 0; len < target.Length; len++)
+            {
+                Console.Write(target[len] + " , ");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
