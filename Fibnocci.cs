ad35ec58889eb9094b5b7a102a5ae830fc5d541e
jasminjol <48532317+jasminjol@users.noreply.github.com>
2019-04-15 15:46:02 -0700

    Add files via upload

---
+++ Fibnocci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibnocci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int prev = 0;
+            int next = 1;
+
+            Console.Write("{0} {1}", prev, next);
+
+            do
+            {
+                number = prev + next;
+                Console.Write(" {0}", number);
+                prev = next;
+                next = number;
+            } while (number < 100);
+
+            Console.ReadKey();
+        }
+    }
+}
