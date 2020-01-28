e7486f2dcfdaf36ac0fe0ffbdf3055793cfa31a6
jasminjol <48532317+jasminjol@users.noreply.github.com>
2020-01-28 11:43:48 -0800

    Add files via upload

---
+++ Linear/Matrix_CountIslands.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_CountIslands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[,]
+                    {
+                       { 1, 0, 1, 0, 0, 0, 1, 1, 1, 1 },
+                       { 0, 0, 1, 0, 1, 0, 1, 0, 0, 0 },
+                       { 1, 1, 1, 1, 0, 0, 1, 0, 0, 0 },
+                       { 1, 0, 0, 1, 0, 1, 0, 0, 0, 0 },
+                       { 1, 1, 1, 1, 0, 0, 0, 1, 1, 1 },
+                       { 0, 1, 0, 1, 0, 0, 1, 1, 1, 1 },
+                       { 0, 0, 0, 0, 0, 1, 1, 1, 0, 0 },
+                       { 0, 0, 0, 1, 0, 0, 1, 1, 1, 0 },
+                       { 1, 0, 1, 0, 1, 0, 0, 1, 0, 0 },
+                       { 1, 1, 1, 1, 0, 0, 0, 1, 1, 1 },
+                    };
+            int X = 10;
+            int Y = 10;
+
+            CountIsland obj = new CountIsland();
+            int islands = 0;
+            bool[,] processed = new bool[X, Y];
+
+            for (int i = 0; i < X; i++)
+            {
+                for (int j = 0; j < Y; j++)
+                {
+                    if (!processed[i, j] && matrix[i,j] == 1)
+                    {
+                        obj.BFS(matrix, i, j, processed);
+                        islands++;
+                    }
+                }
+            }
+            Console.WriteLine("Number of islands : {0}", islands);
+            Console.ReadKey();
+        }
+    }
+
+    public class Cell
+    {
+        public int x;
+        public int y;
+
+        public Cell(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+
+    public class CountIsland
+    {
+        private static readonly int[] row = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        private static readonly int[] col = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+        public bool IsValidPath(int[,] matrix, int x, int y, bool[,] processed)
+        {
+            return ((x >= 0) && (x < 10) && (y >= 0) && (y < 10) && (matrix[x, y] == 1) && !processed[x, y]);
+        }
+
+        public void BFS(int[,] matrix, int x, int y, bool[,] processed)
+        {
+            Queue<Cell> queue = new Queue<Cell>();
+
+            processed[x, y] = true;
+            queue.Enqueue(new Cell(x, y));
+
+            while (queue.Count > 0)
+            {
+                int p = queue.Peek().x;
+                int q = queue.Peek().y;
+                queue.Dequeue();
+
+                for (int k = 0; k < 8; k++)
+                {
+                    if (IsValidPath(matrix, p + row[k], q + col[k], processed))
+                    {
+                        processed[p + row[k], q + col[k]] = true;
+                        queue.Enqueue(new Cell(p + row[k], q + col[k]));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Linear/ShortestPathinMaze_Backtracking.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestPath_Backtracking
+{
+
+    public class Maze
+    {
+        public int Rows = 10;
+        public int Cols = 10;
+
+        public int[] xTrav = { -1, 0, 0, 1 };
+        public int[] yTrav = { 0, -1, 1, 0 };
+
+        public bool IsValid(int x, int y)
+        {
+            return ((x >= 0) && (x < Rows) && (y >= 0) && (y < Cols));
+        }
+
+        public bool IsSafe(int[,] matrix, int[,] visited, int x, int y)
+        {
+            return !(matrix[x,y] == 0 || visited[x,y] != 0);
+        }
+
+        public int ShortestPath(int[,] matrix, int[,] visited, int i, int j, int x, int y, int minDist, int dist)
+        {
+            if (i == x && j == y)
+                return Math.Min(minDist, dist);
+
+            visited[i, j] = 1;
+
+                if (IsValid(i-1, j) && IsSafe(matrix, visited, i-1, j))
+                {
+                    minDist = ShortestPath(matrix, visited, i-1, j, x, y, minDist, dist + 1);
+                }
+
+            if (IsValid(i, j-1) && IsSafe(matrix, visited, i, j-1))
+            {
+                minDist = ShortestPath(matrix, visited, i, j - 1, x, y, minDist, dist + 1);
+            }
+
+            if (IsValid(i, j+1) && IsSafe(matrix, visited, i, j + 1))
+            {
+                minDist = ShortestPath(matrix, visited, i, j + 1, x, y, minDist, dist + 1);
+            }
+
+            if (IsValid(i + 1, j) && IsSafe(matrix, visited, i + 1, j))
+            {
+                minDist = ShortestPath(matrix, visited, i + 1, j, x, y, minDist, dist + 1);
+            }
+
+            visited[i, j] = 0;
+
+            return minDist;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix =
+		{
+				{ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 },
+				{ 0, 1, 1, 1, 1, 1, 0, 1, 0, 1 },
+				{ 0, 0, 1, 0, 1, 1, 1, 0, 0, 1 },
+				{ 1, 0, 1, 1, 1, 0, 1, 1, 0, 1 },
+				{ 0, 0, 0, 1, 0, 0, 0, 1, 0, 1 },
+				{ 1, 0, 1, 1, 1, 0, 0, 1, 1, 0 },
+				{ 0, 0, 0, 0, 1, 0, 0, 1, 0, 1 },
+				{ 0, 1, 1, 1, 1, 1, 1, 1, 0, 0 },
+				{ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 },
+				{ 0, 0, 1, 0, 0, 1, 1, 0, 0, 1 },
+		};
+
+            int[,] visited = new int[10, 10];
+            Maze maze = new Maze();
+
+            int result = maze.ShortestPath(matrix, visited, 0, 0, 7, 5, Int32.MaxValue, 0);
+            if (result != Int32.MaxValue)
+                Console.WriteLine("Shortest path in maze is {0}", result.ToString());
+            else
+                Console.WriteLine("Shortest path not found");
+
+            Console.ReadLine();
+        }
+    }
+}
