c060c0b8fc9531dff880d1eecbb69b4cfab05fde
jasminjol <48532317+jasminjol@users.noreply.github.com>
2020-01-28 11:38:23 -0800

    Add files via upload

---
+++ Hierarchical/DLLToBinaryTree.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLToBT
+{
+    public class Node
+    {
+        public int data;
+        public Node next, prev;
+
+        public Node (int value)
+        {
+            this.data = value;
+            next = prev = null;
+        }
+    }
+
+    public class DLLToBT
+    {
+        public static Node head = null;
+        public static Node tail = null;
+        public static int size = 0;
+        public Node root;
+
+        public void add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                head.prev = node;
+                node.next = head;
+                head = node;
+            }
+
+            size++;
+        }
+        public Node ConvertToBT(int size)
+        {
+            if (size <= 0)
+                return null;
+
+            Node left = ConvertToBT(size / 2);
+            Node root = head;
+            root.prev = left;
+            head = head.next;
+            root.next = ConvertToBT(size - (size / 2) - 1);
+            return root;
+        }
+
+        public void PrintDLL(Node head)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.data + " -> ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+        public void PrintInOrder(Node tree)
+        {
+            if (tree != null)
+            {
+                PrintInOrder(tree.prev);
+                Console.Write(tree.data + " -> ");
+                PrintInOrder(tree.next);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DLLToBT dll = new DLLToBT();
+            dll.add(7);
+            dll.add(6);
+            dll.add(5);
+            dll.add(4);
+            dll.add(3);
+            dll.add(2);
+            dll.add(1);
+            Console.WriteLine("Print DLL");
+            Node h = DLLToBT.head;
+            dll.PrintDLL(h);
+            Console.ReadKey();
+
+            Console.WriteLine("DLL to Binary Tree");
+            Node tree = dll.ConvertToBT(DLLToBT.size);
+            
+            dll.PrintInOrder(tree);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Hierarchical/Graph Dijkstras Shortest Path.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_Dijkstras_Shortest_Path
+{
+
+    public class Dijkstra
+    {
+        public static int V = 9;
+
+        public int MinDistance(int[] dist, bool[] sptSet)
+        {
+            // Initialize min value 
+            int min = int.MaxValue, min_index = -1;
+
+            for (int v = 0; v < V; v++)
+                if (sptSet[v] == false &&
+                      dist[v] <= min)
+                {
+                    min = dist[v];
+                    min_index = v;
+                }
+
+            return min_index;
+        }
+        public void FindShortestPath(int[,] graph, int source)
+        {
+            int[] dist = new int[V];
+            bool[] sptSet = new bool[V];
+
+            for (int i = 0; i < V; i++)
+            {
+                dist[i] = int.MaxValue;
+                sptSet[i] = false;
+            }
+
+            dist[source] = 0;
+
+            // Find shortest path for all vertices 
+            for (int count = 0; count < V-1; count++)
+            {
+                int u = MinDistance(dist, sptSet);
+
+                sptSet[u] = true;
+
+                // Update dist value of the adjacent vertices of the picked vertex.              
+                for (int v = 0; v < V; v++)
+
+                    // Update dist[v] only if is not in  
+                    // sptSet, there is an edge from u  
+                    // to v, and total weight of path  
+                    // from src to v through u is smaller  
+                    // than current value of dist[v] 
+                    if (!sptSet[v] && graph[u, v] != 0 &&
+                               dist[u] != int.MaxValue &&
+                         dist[u] + graph[u, v] < dist[v])
+                        dist[v] = dist[u] + graph[u, v];
+            }
+
+            PrintPath(dist);
+        }        
+
+        public void PrintPath(int[] dist)
+        {
+            Console.Write("Vertex     Distance from Source\n");
+            for (int i = 0; i < V; i++)
+                Console.Write(i + " \t\t " +
+                            dist[i] + "\n");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] graph = new int[,]{{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                          {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                          {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                          {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                          {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                          {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                          {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                          {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                          {0, 0, 2, 0, 0, 0, 6, 7, 0}};
+
+            Dijkstra dijkstra = new Dijkstra();
+            dijkstra.FindShortestPath(graph, 0);
+            Console.ReadKey();
+        }
+    }
+}
