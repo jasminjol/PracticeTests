da854377ef83f87d78e4e511940dfb932bad86c9
jasminjol <48532317+jasminjol@users.noreply.github.com>
2021-01-29 22:11:05 -0800

    Update FindAllCombinationsThatAddsUptoGivenNumber.cs

--- Linear/FindAllCombinationsThatAddsUptoGivenNumber.cs
+++ Linear/FindAllCombinationsThatAddsUptoGivenNumber.cs
@@ -1,4 +1,33 @@
-﻿using System;
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        List<IList<int>> result = new List<IList<int>>();
+        List<int> combination = new List<int>();
+        Array.Sort(candidates);
+        CombinationSum(result, candidates, combination, target, 0);
+        return result;
+    }
+
+    private void CombinationSum(IList<IList<int>> result, int[] candidates, IList<int> combination, int target, int start)
+    {
+        if (target == 0)
+        {
+            result.Add(new List<int>(combination));
+            return;
+        }
+
+        for (int i = start; i != candidates.Length && target >= candidates[i]; ++i)
+        {
+            combination.Add(candidates[i]);
+            CombinationSum(result, candidates, combination, target - candidates[i], i);
+            combination.Remove(combination.Last());
+        }
+    }
+
+
+
+Solution #2
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
